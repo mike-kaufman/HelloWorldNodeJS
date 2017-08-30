9a81ed8bcd642187e89b71aecd1587c2e000ea8d
Mike Kaufman <mike.kaufman@microsoft.com>
2017-08-30 15:31:48 -0700

    reading env var

--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,9 @@ const process = require('process');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  const envVal = process.env('EXTRAINFO') || 'undefined';
-  res.render('index', { title: 'Express', extraInfo: `` });
+  const envVal = process.env['EXTRAINFO'] || 'undefined';
+  const extra = `EXTRAINFO is ${envVal}`;
+  res.render('index', { title: 'Express', extraInfo: extra });
 });
 
 module.exports = router;
